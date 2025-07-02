b6411381d4418a14904bd4ff2641d554173afdb4
NyasaLuharuka <73618884+NyasaLuharuka@users.noreply.github.com>
2025-07-02 19:57:02 -0400

    using nodejs w/ mongo

---
+++ index.js
@@ -0,0 +1,36 @@
+const express=require("express")
+const app=express()
+const path=require("path")
+const hbs=require("hbs")
+const collection = require("./mongodb")
+
+const templatePath = path.join(__dirname, '../templates')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.set("view engine", "hbs")
+app.set("views",templatePath)
+
+app.get("/", (req, res)=>{
+    res.render("login")
+})
+
+app.get("/signup", (req, res)=>{
+    res.render("signup")
+})
+
+
+app.post("/login", async (req, res)=>{
+const data={
+    email:req.body.emailinput,
+    password:req.body.passwordinput
+}
+
+await collection.insertMany([data])
+
+res.render("home")
+})
+
+app.listen(3000, ()=> {
+    console.log("port connected")
+})
