680dfca772dc51423bc6e9ec102f435be99a5670
NyasaLuharuka <73618884+NyasaLuharuka@users.noreply.github.com>
2025-07-08 11:10:02 -0400

    updated fall section

--- fall.js
+++ fall.js
@@ -1,10 +1,23 @@
 function addLeaf() {
-    const text = document.getElementById("leafInput").ariaValueMax;
-    const leaf = document.createElement('div')
+    const input = document.getElementById('leafInput');
+    if (!input.value) return;
+    const leaf = document.createElement('div');
     leaf.className = 'leaf';
-    leaf.textContent = text;
-    leaf.style.left = Math.random() * 90 + "%";
-    document.getElementById("tree").appendChild(leaf);
+    leaf.textContent = "🍁 " + input.value;
+    leaf.style.left = Math.random() * 80 + '%';
+    document.getElementById('tree').appendChild(leaf);
+    input.value = '';
     setTimeout(() => leaf.remove(), 5000);
-    console.log("success");
-}
+  }
+
+  function releaseLantern() {
+    const input = document.getElementById('lanternInput');
+    if (!input.value) return;
+    const lantern = document.createElement('div');
+    lantern.className = 'lantern';
+    lantern.textContent = input.value;
+    lantern.style.left = Math.random() * 90 + '%';
+    document.getElementById('lantern').appendChild(lantern);
+    input.value = '';
+    setTimeout(() => lantern.remove(), 7000);
+  }
